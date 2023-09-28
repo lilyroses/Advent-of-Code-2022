5691d079e5f41673689e76b6686bb841841d1d04
Lily Rose Stracke <59921362+lilyroses@users.noreply.github.com>
2023-09-27 20:03:42 -0700

    Update solution.py

--- solutions/day_6/solution.py
+++ solutions/day_6/solution.py
@@ -8,41 +8,39 @@ with open(INPUT_FILE, 'r') as f:
 
 # PART I
 
-def find_packet_stream_start(stream, x):
-    """Find the first chunk of four letters with no repeating letters."""
-
-    # Using stream[i:j] gives us a chunk of letters at a time
-    i = 0
-    j = x
+def find_packet_stream_start(stream: str, chunk_size: int) -> int:
+    """Find the first chunk of specified number of letters with no repeating letters."""
     
-    # Once j is one less than the length of the stream, we have
-    # reached the final character in the stream's index
+    # Setting j to initial value of chunk_size keeps chunk size the same when adding 1
+    # to both i and j to check all of the stream's characters. 
+    i = 0
+    j = chunk_size # Saves chunk_size for all iterations
+    
+    # Once j equals one less than the number of characters in the stream, we have
+    # reached the index of the final character in the stream. 
     while j < len(stream):
+        # For chunk_size 4, chunk will be stream[0:4], stream[1:5], etc. 
         chunk = stream[i:j]
 
-        # Save unique letters in each chunk to this string
+        # Save only unique letters in each chunk to this string. 
         uniques = ''
-        # Iterate through the letters in the chunk
+        # Iterate through the letters in the current chunk. 
         for letter in chunk:
             if letter not in uniques:
                 uniques += letter
 
-        # Letters are only saved to uniques if they are unique,
-        # so if uniques does not have j letters, there was a
-        # repeating letter
-        if len(uniques) == x:
-            # return final index of the stream where chunk was found
+        # If the number of letters in uniques was not found. 
+        if len(uniques) == chunk_size:
+            # Return the final index of the stream where the chunk was found.
             return j
 
         # If the chunk was not found, add 1 to both j and i to iterate
-        # through the whole stream
+        # through the whole stream.
         i += 1
         j += 1
 
-    # If no unique chunk found
-    return -1
-
-
+    # Executes only if no unique chunk is found. 
+    return False
 
 
 if __name__ == '__main__':        
